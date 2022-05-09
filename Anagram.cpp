d098d31ad014a77682df17e004f392a58a0333ae
aDistarction12 <Shivam12703@gmail.com>
2022-05-09 21:44:40 +0530

    First Commit

---
+++ Anagram.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>  
+using namespace std;
+
+
+string str1;
+string str2;
+void areanargams(string str1, string str2)
+{
+	std::cout << "Enter the first word" << std::endl;
+	cin >> str1;
+	std::cout << "Enter the second word" << std::endl;
+	cin >> str2;
+
+	int n1 = str1.length();
+	int n2 = str2.length();
+
+	sort(str1.begin(), str1.end());
+	sort(str2.begin(), str2.end());
+		
+	if (str1 == str2)
+	{
+		std::cout << "Strings are anargams" << std::endl;
+	}
+	else
+	{
+		std::cout << "Strings are not anargams" << std::endl;
+	}
+}
+
+int main()
+{
+	areanargams(str1, str2);
+}
